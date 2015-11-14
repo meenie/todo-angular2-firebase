8b217e317ed7bf5ccedd400e11ffe0704867caf4
Richard Park <objectiv@gmail.com>
2015-11-14 00:38:52 -0500

    chore: update test configuration
    - karma: add proxy settings to enable loading of component `templateUrl` and `styleUrls`
    - specs: remove unnecessary `main()` function wrapper from spec files
    - firebase: move mockfirebase and configure karma to load it as window global
    - typings: add custom typing for mockfirebase to suppress compilation error message
    - gulp, karma, npm: remove unnecessary/outdated dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -20,10 +20,8 @@ var paths = {
     src: [
       'node_modules/angular2/bundles/angular2.min.js',
       'node_modules/angular2/bundles/router.dev.js', //{min.js,min.js.map}',
-      'node_modules/es6-module-loader/dist/es6-module-loader.{js,js.map}',
       'node_modules/es6-shim/es6-shim.{map,min.js}',
       'node_modules/firebase/lib/firebase-web.js',
-      'node_modules/immutable/dist/immutable.min.js',
       'node_modules/systemjs/dist/system.{js,js.map}'
     ],
     target: 'target/lib'
